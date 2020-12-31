3c6aa69dfb577a67d2a6ea97a3c6d597b718c21c
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-12-31 15:27:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preload() {
     img = loadImage('dog_cat.jpg');
@@ -7,6 +8,21 @@ function preload() {
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+console.log(results);
 }
 
 function draw() {
@@ -16,4 +32,9 @@ function draw() {
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
 }
